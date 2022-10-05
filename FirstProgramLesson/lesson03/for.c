061655b0ebe8afe73152e86fef147fff5b3c4488
qixia1998 <qixia717138552@gmail.com>
2022-10-06 00:32:52 +0800

    Updated lesson03

---
+++ FirstProgramLesson/lesson03/for.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+
+    for (int i = 1, k = 0; i <= 48; i++, k += 2) printf("%d\n", k);
+}
---
+++ FirstProgramLesson/lesson03/if_else.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d%d", &a, &b);
+
+    if (a == b) printf("a is equal to b!\n");
+    // if (a != b) printf("a is not equal to b!\n");
+    // if (a != b) printf("a is not equal to b!\n");
+    else printf("a is not equal to b!\n");
+
+    return 0;
+}
