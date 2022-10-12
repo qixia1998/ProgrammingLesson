3ace18c349ee7f528f18b0080312142250d4f008
qixia1998 <qixia717138552@gmail.com>
2022-10-12 23:36:34 +0800

    Updated lesson07

---
+++ FirstProgramLesson/lesson07/pointer.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    int a = 123, *p = &a;
+    printf("%d %p %d\n", a, p, *p);
+}
---
+++ FirstProgramLesson/lesson07/struct_data.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    struct Data {
+        int x, y;
+    } a[2];
+
+    struct Data *p = p;
+    printf("%p", &a[1].x);
+    return 0;
+}
